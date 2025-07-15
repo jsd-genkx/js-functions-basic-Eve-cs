ff3564d2ed53f20d9d7b388092e736b4eca9f123
eve.cs <eve.chunyanan.s@gmail.com>
2025-07-15 19:53:57 +0700

    task04 solution

--- task04.js
+++ task04.js
@@ -17,6 +17,21 @@ Task 04: Traffic Light Action
 */
 
 // 👨‍💻 TODO-01: Write your code below to complete the task as described above.
+const trafficLightAction = (colorLight) => {
+  switch (colorLight) {
+    case "green":
+      return "Go";
+      break;
+    case "yellow":
+      return "Slow down";
+      break;
+    case "red":
+      return "Stop";
+      break;
+    default:
+      return "Invalid light color";
+  }
+};
 
 // Example Test Cases
 console.log(trafficLightAction("green")); // "Go"
